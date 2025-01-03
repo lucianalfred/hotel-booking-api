90d891c7ab65514b2d01f060be512988cfe80f24
Luciano Alfredo <lucianalfred60@gmail.com>
2025-01-03 23:05:01 +0100

    Room model

--- src/models/Hotels.js
+++
@@ -1,9 +0,0 @@
-const {DataTypes} = require("sequelize");
-const sequelize = require('../config/database');
-
-const Hotel = sequelize.define('Hotel',{
-    name: {type:DataTypes.STRING, allowNull:false},
-    address:{type:DataTypes.STRING, allowNull:false},
-});
-
-module.exports = Hotel;
